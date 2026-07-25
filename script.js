ffb2cc8832d4a2bc37626201f257eac42d1417f0
anilkumar2621 <anilkumarjadi25@gmail.com>
2026-07-25 14:34:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+=========================================
+FM STORE-STYLES
+Main JavaScript
+=========================================
+*/
+
+const APP = {
+    apiUrl: "",
+    version: "1.0.0"
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("FM STORE-STYLES Loaded");
+});
